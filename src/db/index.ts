132191192fbdc4c5869c4a333b425dc60f3c0017
Zakia Sultana <128232277+ZakiaSultana4@users.noreply.github.com>
2024-12-05 18:24:33 +0600

    Update index.ts

--- src/db/index.ts
+++ src/db/index.ts
@@ -3,7 +3,7 @@ import config from "../config";
 
 const connectDB = async () => {
   try {
-     await mongoose.connect(`${config.database_url}`);
+    await mongoose.connect(`${config.database_url}`);
     console.log("server is connected to mongoDB ");
   } catch (err) {
     console.log("MongoDB connection error", err);
