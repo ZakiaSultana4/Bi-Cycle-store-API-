556bb690a0ee5e502fcf4be1552b080fef61ab87
Zakia Sultana <128232277+ZakiaSultana4@users.noreply.github.com>
2024-12-05 18:32:56 +0600

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,6 @@ connectDB()
       console.log(`Example app listening on port ${config.port || 3000}`);
     });
   })
-
   .catch((error) => {
     console.log("mongoDB connection failed ", error);
   });
